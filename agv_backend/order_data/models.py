964d736efd4900fa5ee6d6b3ab1b7b14b98788b6
sondhg <sondhw@gmail.com>
2024-10-24 00:06:32 +0700

    add model fields load_amount and load_weight

--- agv_backend/order_data/models.py
+++ agv_backend/order_data/models.py
@@ -8,7 +8,8 @@ class Order(models.Model):
     start_point = models.IntegerField()
     end_point = models.IntegerField()
     load_name = models.CharField(max_length=100)
-    load_weight = models.IntegerField()
+    load_amount = models.IntegerField(default=0)
+    load_weight = models.IntegerField(default=0)
 
     def __str__(self):
         return f"Order {self.order_id} has been created"
