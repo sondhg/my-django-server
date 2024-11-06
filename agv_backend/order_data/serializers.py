0703c72b0fab89bc83f86afdbc66b81822211579
sondhg <sondhw@gmail.com>
2024-11-06 16:25:51 +0700

    remove custom date and time formats from OrderSerializer

--- agv_backend/order_data/serializers.py
+++ agv_backend/order_data/serializers.py
@@ -4,13 +4,6 @@ from .models import Order
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    order_date = serializers.DateField(
-        format="%m/%d/%Y", input_formats=["%m/%d/%Y"]  # Capital "Y" means 4-digit year
-    )
-    start_time = serializers.TimeField(
-        format="%H:%M:%S", input_formats=["%H:%M:%S"]
-    )  # Ensure correct format
-
     class Meta:
         model = Order
         fields = "__all__"
