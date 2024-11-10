9b188f410328480c54de68ab35ac4d05424de9ad
sondhg <sondhw@gmail.com>
2024-11-10 23:49:55 +0700

    alter User model to enforce unique constraint on name field and update migration for the change

--- agv_backend/users/models.py
+++ agv_backend/users/models.py
@@ -6,11 +6,11 @@ from django.db import models
 
 # Create your models here.
 class User(AbstractUser):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
     username = None
-    refresh_token = models.CharField(max_length=255, null=True, blank=True)  # Add this line
+    refresh_token = models.CharField(max_length=255, null=True, blank=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
