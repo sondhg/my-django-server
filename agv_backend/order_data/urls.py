609e402e263e7cf1a6eb820fdbda91ec9bc252fa
sondhg <sondhw@gmail.com>
2024-11-10 22:57:26 +0700

    refactor order_data API to use viewsets and router for improved routing and bulk creation support. pay attention to trailing slash at the end of API endpoint

--- agv_backend/order_data/urls.py
+++ agv_backend/order_data/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import OrderView
 
-from . import views
+router = DefaultRouter()
+router.register(r'orders', OrderView, basename='order') # localhost:8000/api/orders/
 
 urlpatterns = [
-    path("orders/", views.order_list, name="order_list"),
-    path("orders/<int:id>/", views.order_detail, name="order_detail"),
+    path('', include(router.urls)),
 ]
--- agv_backend/order_data/views.py
+++ agv_backend/order_data/views.py
@@ -1,51 +1,24 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework import status, viewsets
 from rest_framework.response import Response
 
 from .models import Order
 from .serializers import OrderSerializer
 
-@api_view(["GET", "POST"])
-def order_list(request):
-    if request.method == "GET":
-        orders = Order.objects.all().order_by('order_id')  # Sort by order_id
-        serializer = OrderSerializer(orders, many=True)
-        return Response(serializer.data)
 
-    elif request.method == "POST":
-        # Check if request.data is a list (for bulk creation)
+class OrderView(viewsets.ModelViewSet):
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
+
+    def create(self, request):
         if isinstance(request.data, list):
-            # Set many=True to handle multiple objects
+            # Handle multiple objects
             serializer = OrderSerializer(data=request.data, many=True)
         else:
-            serializer = OrderSerializer(data=request.data)  # Single object
+            # Handle single object
+            serializer = OrderSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(["GET", "PUT", "DELETE"])
-def order_detail(request, id):
-    try:
-        order = Order.objects.get(
-            order_id=id
-        )  # ! because order_id is the primary key, use order_id=id instead of id=id
-    except Order.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == "GET":
-        serializer = OrderSerializer(order)
-        return Response(serializer.data)
-
-    elif request.method == "PUT":
-        serializer = OrderSerializer(order, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == "DELETE":
-        order.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
